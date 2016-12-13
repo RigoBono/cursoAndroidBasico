8f996eda47e1034abf4264e1d172654b2277191b
RigoBono <Rigo1994>
2016-12-13 12:20:48 -0600

    practica 1 dia 2

---
+++ dia2/Dia2/app/src/main/java/com/eldia2/dia2/adapter/adapterUsr.java
@@ -0,0 +1,8 @@
+package com.eldia2.dia2.adapter;
+
+/**
+ * Created by rigobono on 13/12/16.
+ */
+
+public class adapterUsr {
+}
--- dia2/Dia2/app/src/main/java/com/eldia2/dia2/model/usuario.java
+++ dia2/Dia2/app/src/main/java/com/eldia2/dia2/model/usuario.java
@@ -5,4 +5,37 @@ package com.eldia2.dia2.model;
  */
 
 public class usuario {
+    String nombre;
+    int edad;
+    String escuela;
+
+    public usuario(String nombre, int edad, String escuela) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.escuela = escuela;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEscuela() {
+        return escuela;
+    }
+
+    public void setEscuela(String escuela) {
+        this.escuela = escuela;
+    }
 }
