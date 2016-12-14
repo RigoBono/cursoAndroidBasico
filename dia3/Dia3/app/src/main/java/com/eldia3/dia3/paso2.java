9810b2b9232bf07d7084f1dbdeb1213ef9d75ec9
RigoBono <Rigo1994>
2016-12-14 09:28:42 -0600

    practica dia 3

---
+++ dia3/Dia3/app/src/main/java/com/eldia3/dia3/paso2.java
@@ -0,0 +1,13 @@
+package com.eldia3.dia3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class paso2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paso2);
+    }
+}
