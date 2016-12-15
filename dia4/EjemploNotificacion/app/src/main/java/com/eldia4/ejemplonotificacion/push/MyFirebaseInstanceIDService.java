d503c866c1920ec58399dcc0e3ba9580687808b8
RigoBono <Rigo1994>
2016-12-15 11:22:02 -0600

    Firebase push

---
+++ dia4/EjemploNotificacion/app/src/main/java/com/eldia4/ejemplonotificacion/push/MyFirebaseInstanceIDService.java
@@ -0,0 +1,36 @@
+package com.eldia4.ejemplonotificacion.push;
+
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by rigobono on 15/12/16.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.i(TAG, "Refreshed token: " + refreshedToken);
+
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        final Intent intent = new Intent("tokenReceiver");
+        // You can also include some extra data.
+        final LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(this);
+        intent.putExtra("token",token);
+        broadcastManager.sendBroadcast(intent);
+
+    }
+
+
+}
---
+++ dia4/EjemploNotificacion/app/src/main/java/com/eldia4/ejemplonotificacion/push/MyFirebaseMessagingService.java
@@ -0,0 +1,8 @@
+package com.eldia4.ejemplonotificacion.push;
+
+/**
+ * Created by rigobono on 15/12/16.
+ */
+
+public class MyFirebaseMessagingService {
+}
