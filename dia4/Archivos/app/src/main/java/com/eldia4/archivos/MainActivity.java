1ac10c9412394a76ff457bd6559d68cad0e882dc
RigoBono <Rigo1994>
2016-12-15 10:04:25 -0600

    Archivos

--- dia4/Archivos/app/src/main/java/com/eldia4/archivos/MainActivity.java
+++ dia4/Archivos/app/src/main/java/com/eldia4/archivos/MainActivity.java
@@ -9,15 +9,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
     Button creaArchivos;
+    Button leerArchivos;
+    TextView contenido;
 
 
     @Override
@@ -27,11 +35,21 @@ public class MainActivity extends AppCompatActivity {
         checapermisos();
 
         creaArchivos=(Button)findViewById(R.id.creaArchivos);
+        leerArchivos=(Button)findViewById(R.id.leerArchivos);
+        contenido=(TextView) findViewById(R.id.contenido);
 
         creaArchivos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 escribeArchivo();
+            }
+        });
+
+
+        leerArchivos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                leerArchivo();
             }
         });
     }
@@ -44,10 +62,14 @@ public class MainActivity extends AppCompatActivity {
     public void checapermisos()
     {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED)
+                != PackageManager.PERMISSION_GRANTED  &&
+
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED
+                )
         {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},
                     1);
             return;
         }
@@ -76,4 +98,24 @@ public class MainActivity extends AppCompatActivity {
             Log.e("Ficheros", "Error al escribir fichero a tarjeta SD");
         }
     }
+
+    public void leerArchivo(){
+        try
+        {
+            File ruta_sd = Environment.getExternalStorageDirectory();
+            File f = new File(ruta_sd.getAbsolutePath(), "prueba_sd.txt");
+            BufferedReader fin = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+            String texto = fin.readLine();
+            contenido.setText(texto);
+            fin.close();
+
+            Toast.makeText(MainActivity.this,"Lectura correcta",Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception ex)
+        {
+            Log.e("Ficheros", "Error al leer fichero desde tarjeta SD");
+        }
+    }
+
+
 }
